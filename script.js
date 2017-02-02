99a84305ae4417b7cd0722aac41dc239ac177f13
superbuggy <jpqreichard@gmail.com>
2017-02-02 18:19:41 -0500

    adds solution to fourth exercise

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ swatchDivs.mouseover(function(){
   $("#output").html(thisBgColor)
 })
 
-swatchDivs.hover(
+// I've refactored exercise 2 solution here to get rid of
+// the buggy display behavior
+swatchDivs.mouseenter(
   function(){
-    $(this).fadeOut()
-  },
-  function(){
-    $(this).fadeIn()
+    $(this).fadeOut().fadeIn()
   }
 )
 
+// exercise 3 solution
 $("form").submit(function(evt){
   evt.preventDefault();
   var colorInput = $("form input").eq(0).val();
@@ -26,4 +26,39 @@ $("form").submit(function(evt){
     newDiv.css("background-color", colorInput);
     $(".container").append(newDiv);
   }
+})
+
+// exercise 4 solution
+
+//we're declaring these outside of the callback function below
+//so that the program can 'remember' what these values are outside
+// of the ***scope*** of that function. otherwise, they would be
+// garbage-collected, meaning that the variables inside the function 'die' in a sense
+var clickCounter = 0;
+var divClickedFirst;
+var divClickedSecond;
+swatchDivs.click(function(){
+
+  //increments click counter
+  clickCounter++;
+
+  // the reason we are checking for an even or odd number of clicks is to
+  // determine which is the first element clicked on, and the second element
+  // clicked on. the clickCounter starts at 0
+
+  // checks to see if number is odd using the modulus operator %
+  if (clickCounter % 2 == 1) {
+    divClickedFirst = $(this);
+  } else {
+    // else, there's an even number of clicks
+    divClickedSecond = $(this);
+    // we capture the value before we reassign them in separate variables
+    var divClickedFirstBgColor = divClickedFirst.css("background-color");
+    var divClickedSecondBgColor = divClickedSecond.css("background-color");
+
+    //making the swap
+    divClickedFirst.css("background-color", divClickedSecondBgColor)
+    divClickedSecond.css("background-color", divClickedFirstBgColor)
+  }
+
 })
