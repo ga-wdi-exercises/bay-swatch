8773a9c4d10f02789643d383216ecd4493d2a83c
superbuggy <jpqreichard@gmail.com>
2017-02-02 15:54:36 -0500

    add solution for 2nd exercise

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
 var swatchDivs = $(".swatch");
 
 swatchDivs.mouseover(function(){
-  //need to isolate whatever element was clicked
-  //get some css value related to its color
   var thisBgColor = $(this).css("background-color");
-  console.log(thisBgColor);
-  // $("#output").text(thisBgColor) will work also
   $("#output").html(thisBgColor)
+})
+
+//2nd exercise
+swatchDivs.mouseenter(function(){
+  $(this).fadeOut()
+})
+
+swatchDivs.mouseleave(function(){
+  $(this).fadeIn()
 })
